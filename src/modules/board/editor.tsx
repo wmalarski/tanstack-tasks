2a70bb15a5b4ef610319a99db48c8f8897658e20
Wojtek Malarski <w.malarski@gmail.com>
2025-12-27 18:28:53 +0100

    nodes throttled update

--- src/modules/board/editor.tsx
+++ src/modules/board/editor.tsx
@@ -7,7 +7,14 @@ import {
   type NodeChange,
   ReactFlow,
 } from "@xyflow/react";
-import { type ComponentProps, useCallback, useState } from "react";
+import {
+  type ComponentProps,
+  useCallback,
+  useEffect,
+  useEffectEvent,
+  useRef,
+  useState,
+} from "react";
 import "@xyflow/react/dist/style.css";
 
 import { convexQuery } from "@convex-dev/react-query";
@@ -30,18 +37,20 @@ export const Editor = ({ boardId, nodes, edges }: EditorProps) => {
 
   const [isInsertNodeOpen, setIsInsertNodeOpen] = useState(false);
 
+  const throttledUpdate = useThrottledNodesUpdate(nodes);
+
   const onNodesChange = useCallback(
     (changes: NodeChange<NodeResult>[]) => {
+      throttledUpdate(changes);
+
       const nodesQueryOptions = convexQuery(api.nodes.queryNodes, { boardId });
 
       queryClient.setQueryData(
         nodesQueryOptions.queryKey,
         (current: NodeResult[]) => applyNodeChanges(changes, current),
       );
-
-      console.log("[onNodesChange]", changes);
     },
-    [boardId, queryClient.setQueryData],
+    [boardId, queryClient.setQueryData, throttledUpdate],
   );
 
   const onEdgesChange = useCallback(
@@ -95,4 +104,50 @@ export const Editor = ({ boardId, nodes, edges }: EditorProps) => {
       />
     </div>
   );
+};
+
+const useThrottledNodesUpdate = (nodes: NodeResult[]) => {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const changesRef = useRef<NodeChange<NodeResult>[]>([]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return useEffectEvent((updates: NodeChange<NodeResult>[]) => {
+    if (timeoutRef.current) {
+      changesRef.current.push(...updates);
+      return;
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      const changes = changesRef.current;
+      timeoutRef.current = null;
+      changesRef.current = [];
+
+      const changedNodeIds = new Set<string>();
+
+      changes.forEach((change) => {
+        if (change.type !== "add") {
+          changedNodeIds.add(change.id);
+        }
+      });
+
+      const changedNodes = nodes.filter((node) => changedNodeIds.has(node.id));
+
+      const applied = applyNodeChanges(changes, changedNodes);
+
+      console.log("[applied]", {
+        applied,
+        changedNodeIds,
+        changedNodes,
+        changes,
+        nodes,
+      });
+    }, 1000);
+  });
 };
